cf3195af711551f1fa6a31fae69aa2b89bd292e3
Krung <Krung@users.noreply.github.com>
2016-05-24 19:09:49 -0400

    Added Video and Channel classes.
    Added more and clearer comments.
    Added temporary action methods.

--- Example_-_Video_Viewer.py
+++ Example_-_Video_Viewer.py
@@ -1,16 +1,31 @@
 import sys
-from beast.ytmonster import YTMonster
+from beast.ytmonster import YTMonster, Video
 
 if len(sys.argv) == 3:
-  # Create client instance
+
+  # Create the client instance.
   client = YTMonster()
-  # Set the maximum length (seconds) that a video watch request may be
-  client.set_limit(125)
-  # Attempt to login
+  
+  # Set the maximum length (in seconds) that a video watch request may be.
+  client.set_limit(100)
+  
+  # Attempt to login with the supplied credentials.
   client.login(sys.argv[1], sys.argv[2])
   if client.is_online():
-    # Successfully complete watching 25 videos
-    client.start(5)
+    client.start()
+    
+    # Attempt to watch 5 videos.
+    videos = Video(client)
+    watched = 0
+    while watched < 5:
+      if videos.watch():
+        watched += 1
+    
+    # Display the user's current points.
+    client.show_points()
+  
+    # Log out when all actions are complete.
     client.logout()
+
 else:
   print("Usage:\n\n\tExample_-_Video_Viewer.py Username Password")
--- beast/ytmonster.py
+++ beast/ytmonster.py
@@ -4,6 +4,68 @@ from json import loads
 from requests import Session
 from sys import stdout
 
+""" Channel """
+
+class Channel(object):
+  
+  def __init__(self, client):
+    self.client = client
+
+  def subscribe(self):
+    """ Returns whether or not a channel was successfully subscribed to. """
+    payload = { "username" : self.client.get_username() }
+    request = self.client.get_session().get("http://www.ytmonster.net/wSubscribe3.php", params = payload)
+    source = request.text.encode(stdout.encoding, errors="replace")
+  
+""" Video """
+
+class Video(object):
+
+  def __init__(self, client):
+    self.client = client
+  
+  def comment(self):
+    """ Return whether or not a video was successfully commented on. """
+    payload = { "username" : self.client.get_username() }
+    request = self.client.get_session().get("http://www.ytmonster.net/wComment3.php", params = payload)
+    source = request.text.encode(stdout.encoding, errors="replace")
+    
+  def like(self):
+    """ Return whether or not a video was successfully liked. """
+    payload = { "username" : self.client.get_username() }
+    request = self.client.get_session().get("http://www.ytmonster.net/wLike3.php", params = payload)
+    source = request.text.encode(stdout.encoding, errors="replace")
+  
+  def watch(self):
+    """ Return whether or not a video was successfully considered watched. """
+    payload = { "username" : self.client.get_username() }
+    request = self.client.get_session().get("http://www.ytmonster.net/watch4.php", params = payload)
+    source = request.text.encode(stdout.encoding, errors="replace")
+    # Ensure a valid response was received.
+    if b"http://www.ytmonster.net/refDo.php?url=" not in source:
+      return False
+    id = source.split(b'watch?v=')[1].split(b'#')[0].decode("utf-8")
+    # Verify that the watch duration is not longer than the limit set.
+    duration = int(source.split(b'watch?v=')[1].split(b'#')[1])
+    if duration > self.client.get_limit():
+      return False
+    # Send a GET request to the URL the previous request returned.
+    self.client.get_session().get(source)
+    payload["mark"] = id
+    # Simulate watching the video.
+    print("Watching " + id + " for " + str(duration) + "s... ", end="")
+    sys.stdout.flush()
+    time.sleep(duration)
+    # Check for success after telling the server the video was watched.
+    finish = self.client.get_session().get("http://www.ytmonster.net/watch3.php", params = payload)
+    if b"Watched video successfully." in finish.text.encode(stdout.encoding, errors="replace"):
+      print("Succeeded")
+      return True
+    print("Failed")
+    return False
+
+""" YTMonster """
+
 class YTMonster(object):
 
   def __init__(self):
@@ -13,81 +75,52 @@ class YTMonster(object):
     self.username = None
 
   def login(self, username, password):
+    """ Return whether or not a user has successfully logged in. """
     payload = { "usernames" : username, "passwords" : password, "submit" : "" }
     self.session.get("http://www.ytmonster.net/login")
     login = self.session.post("http://www.ytmonster.net/login?login=ok", data = payload)
     source = login.text.encode(stdout.encoding, errors="replace")
     if b"<a href=\"/logout\">Sign out</a>" in source:
       self.username = username
-      print("User '" + username + "' has been logged in.")
+      print("'" + username + "' has been logged in.")
       self.session.headers.update({ 'referer' : "http://www.ytmonster.net/client/" + username })
       self.online = True
     else:
-      print("User '" + username + "' failed to login with the credentials specified.")
+      print("'" + username + "' failed to login with the credentials given.")
       self.online = False
 
   def logout(self):
+    """ Return whether or not a user has successfully logged out. """
     self.session.get("http://www.ytmonster.net/logout")
-    print("Successfully logged out user '" + self.username + "'.")
+    self.session.headers.update({ 'referer' : None })
+    self.online = False
+    print("Successfully logged out '" + self.username + "'.")
+
+  def get_limit(self):
+    return self.limit
+
+  def get_session(self):
+    return self.session
 
   def get_stats(self):
+    """ Return a JSON object of the user statistics. """
     payload = { "username" : self.username }
     gUserStats = self.session.get("http://www.ytmonster.net/data/api3.php", params = payload)
     rUserStats = gUserStats.text.encode(stdout.encoding, errors="replace")
     return loads(rUserStats.decode("utf-8"))
-
+  
+  def get_username(self):
+    return self.username
+    
   def is_online(self):
     return self.online
-    
-  def watch_video(self, id, seconds):
-    time.sleep(int(seconds) + 1)
-    payload = { "username" : self.username, "mark" : id }
-    gWatchedVideo = self.session.get("http://www.ytmonster.net/watch3.php", params = payload)
-    if b"Watched video successfully." in gWatchedVideo.text.encode(stdout.encoding, errors="replace"):
-      return True
-    else:
-      for i in range(2):
-        time.sleep(5)
-        gWatchedVideo = self.session.get("http://www.ytmonster.net/watch3.php", params = payload)
-        if b"Watched video successfully." in gWatchedVideo.text.encode(stdout.encoding, errors="replace"):
-          return True
-      return False
-
-  def get_video(self):
-    payload = { "username" : self.username }
-    gNextVideo = self.session.get("http://www.ytmonster.net/watch4.php", params = payload)
-    rNextVideo = gNextVideo.text.encode(stdout.encoding, errors="replace")
-    if b"http://www.ytmonster.net/refDo.php?url=" in rNextVideo:
-      self.session.get(rNextVideo)
-      rVideoArgs = rNextVideo.split(b'watch?v=')[1]
-      return rVideoArgs.split(b'#')
-    elif b"Already watched." in rNextVideo:
-      #print("Video given has already been seen.")
-      return 0
-    else:
-      print("Error when attempting to get the next video.")
-    return 0
 
   def set_limit(self, limit):
     self.limit = int(limit)
 
-  def start(self, videos = 1):
-    payload = { "username" : self.username }
-    current = 0
+  def show_points(self):
+    print("'" + self.username + "' currently has " + self.get_stats()['credits'] + ".")
+    
+  def start(self):
+    """ Send the GET request in order to begin actions as the client. """
     self.session.get("http://www.ytmonster.net/waitTime.php")
-    self.session.get("http://www.ytmonster.net/wComment3.php", params = payload)
-    self.session.get("http://www.ytmonster.net/wSubscribe3.php", params = payload)
-    self.session.get("http://www.ytmonster.net/wLike3.php", params = payload)
-    while current < videos:
-      nextVid = self.get_video()
-      if nextVid != 0:
-        if int(nextVid[1]) <= self.limit:
-          print("Watching " + nextVid[0].decode("utf-8") + " for " + nextVid[1].decode("utf-8") + "s... ", end="")
-          sys.stdout.flush()
-          if self.watch_video(nextVid[0], nextVid[1]):
-            current += 1
-            print("Success (" + str(current) + "/" + str(videos) + ")")
-            if current % 5 == 0:
-              print("'" + self.username + "' currently has " + self.get_stats()['credits'] + ".")
-          else:
-            print("Failed")
